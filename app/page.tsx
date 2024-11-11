96dcd7dbc972d109b2ea0453fa02aedf64aa64be
Joe-Atteen <joseph.atteen@hubtel.com>
2024-11-11 11:32:40 +0000

    ESLint & Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 async function Home() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/albums');
+  const response = await fetch("https://jsonplaceholder.typicode.com/albums");
   if (!response.ok) {
     throw new Error("Failed to fetch data from the API");
   }
@@ -7,11 +7,11 @@ async function Home() {
   const albums = await response.json();
 
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-black">
-      {albums.map((album: {id:number, title:string}) => (
+    <div className="grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-black sm:p-20">
+      {albums.map((album: { id: number; title: string }) => (
         <div key={album.id} className="text-center">
           <h2 className="text-2xl font-bold">{album.title}</h2>
-          <p>{ album.id}</p>
+          <p>{album.id}</p>
         </div>
       ))}
     </div>
