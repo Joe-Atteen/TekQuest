28332d7eb4f9e3a769aa7e9e46c25fbb1560054b
Joe-Atteen <joseph.atteen@hubtel.com>
2025-01-15 14:31:29 +0000

    Render UI states

--- lib/mongoose.ts
+++ lib/mongoose.ts
@@ -1,5 +1,8 @@
 import mongoose, { Mongoose } from "mongoose";
 
+import logger from "./logger";
+import "@/database";
+
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
@@ -24,6 +27,7 @@ if (!cached) {
 
 const dbConnect = async (): Promise<Mongoose> => {
   if (cached.conn) {
+    logger.info("Using existing mongoose connection");
     return cached.conn;
   }
 
@@ -33,11 +37,11 @@ const dbConnect = async (): Promise<Mongoose> => {
         dbName: "devflow",
       })
       .then((result) => {
-        console.log("Connected to MongoDB");
+        logger.info("Connected to MongoDB");
         return result;
       })
       .catch((error) => {
-        console.error("Error connecting to MongoDB", error);
+        logger.error("Error connecting to MongoDB", error);
         throw error;
       });
   }
