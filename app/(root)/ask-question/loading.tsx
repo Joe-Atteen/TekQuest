758e2ced7f96dc589983df4b80b280e3a034bf1d
Joe Atteen <57702368+Joe-Atteen@users.noreply.github.com>
2025-05-03 23:35:02 +0000

    loader update

--- app/(root)/ask-question/loading.tsx
+++ app/(root)/ask-question/loading.tsx
@@ -3,7 +3,7 @@ const Loading = () => {
     <section>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
-      <div className="flex h-[calc(100dvh-400px)] items-center justify-center">
+      <div className="flex h-[calc(100dvh-200px)] items-center justify-center">
         <div className="size-14 animate-spin rounded-full border-t-4 border-orange-600"></div>
       </div>
     </section>
