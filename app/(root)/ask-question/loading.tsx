374487a2d26c810cfa881aa64aa4f19dc1ec7e65
Joe Atteen <57702368+Joe-Atteen@users.noreply.github.com>
2025-05-03 23:07:12 +0000

    Ask a question loader

--- app/(root)/ask-question/loading.tsx
+++ app/(root)/ask-question/loading.tsx
@@ -5,15 +5,8 @@ const Loading = () => {
     <section>
       <h1 className="h1-bold text-dark100_light900">Ask a question</h1>
 
-      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
-        <Skeleton className="h-14 flex-1" />
-        <Skeleton className="h-14 w-28" />
-      </div>
-
-      <div className="mt-11 flex flex-wrap gap-5">
-        {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((item) => (
-          <Skeleton key={item} className="h-[11.5rem] w-full rounded-xl" />
-        ))}
+      <div className="flex h-[calc(100dvh-400px)] items-center justify-center">
+        <div className="size-14 animate-spin rounded-full border-t-4 border-orange-600"></div>
       </div>
     </section>
   );
